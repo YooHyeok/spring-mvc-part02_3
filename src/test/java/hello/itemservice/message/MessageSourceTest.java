48195693218f14ffe75ba56eaa28b9fb435a4bd2
YooHyeok <yjk7454@naver.com>
2023-04-03 23:51:34 +0900

    테스트코드 notFoundMessageCode() - 메시지 예외 발생 테스트 - noSuch

--- src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -16,7 +16,7 @@ public class MessageSourceTest {
     MessageSource ms;
 
     /**
-     * hello 메시지 조회
+     * hello 메시지 조회 테스트
      * message.properties
      */
     @Test
@@ -24,5 +24,16 @@ public class MessageSourceTest {
         String result = ms.getMessage("hello", null, null);
         //locale정보가 없으면 basename의 기본 이름 메시지 파일인 message.properties를 조회한다.
         assertThat(result).isEqualTo("안녕");
+    }
+
+    /**
+     * 메시지 조회 오류 테스트
+     * assertThatThrownBy() - NoSuchMessageException
+     * 예외 발생 테스트
+     */
+    @Test
+    void notFoundMessageCode() {
+        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class); //녹색불이 들어오면 예외발생 (메시지를 찾을 수 없어서 오류가 발생했다.)
     }
 }
